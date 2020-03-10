f940184affafdfae9be4d88dbb0fd78ad86e6aca
PirWeb <pirmansyah1310@gmail.com>
2020-03-10 14:25:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ document.getElementById('generate').addEventListener('click', function(e){
   let stringArr = input.split('');
   let arr = [];
   for(let i = 0; i < stringArr.length; i++){
-    if(stringArr[i] === "a" || stringArr[i] === "u" || stringArr[i] === "o" || stringArr[i] === "e" || stringArr[i] === "A" || stringArr[i] === "U" || stringArr[i] === "O" || stringArr[i] === "E"){
+    if(stringArr[i] === "a" || stringArr[i] === "u" || stringArr[i] === "o" || stringArr[i] === "e"){
       arr.push("i");
-    }else{
+    }else if( stringArr[i] === "A" || stringArr[i] === "U" || stringArr[i] === "O" || stringArr[i] === "E"){
+      arr.push("I");
+    }
+    else{
       arr.push(stringArr[i]);
     }
   }
