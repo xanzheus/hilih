bfaefe7398809970a2a02760dc1a2e2db3745f2d
PirWeb <pirmansyah1310@gmail.com>
2020-03-10 14:13:45 +0700

    oke

---
+++ app.js
@@ -0,0 +1,19 @@
+document.getElementById('generate').addEventListener('click', function(e){
+  
+  let input = document.getElementById('input').value;
+  
+  let stringArr = input.split('');
+  let arr = [];
+  for(let i = 0; i < stringArr.length; i++){
+    if(stringArr[i] === "a" || stringArr[i] === "u" || stringArr[i] === "o" || stringArr[i] === "e"){
+      arr.push("i");
+    }else{
+      arr.push(stringArr[i]);
+    }
+  }
+  hasil = arr.join('');
+  console.log(hasil);
+  document.querySelector('.output').innerHTML = `<h4>${hasil}</h4>`;
+
+  e.preventDefault();
+});
