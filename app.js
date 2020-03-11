7517c8c0242e6ef626a510f825fbb32d2e560ff5
PirWeb <pirmansyah1310@gmail.com>
2020-03-11 10:59:28 +0700

    finish

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 document.getElementById('generate').addEventListener('click', function(e){
   
   let input = document.getElementById('input').value;
-  
-  let stringArr = input.split('');
-  let arr = [];
-  for(let i = 0; i < stringArr.length; i++){
-    if(stringArr[i] === "a" || stringArr[i] === "u" || stringArr[i] === "o" || stringArr[i] === "e"){
-      arr.push("i");
-    }else if( stringArr[i] === "A" || stringArr[i] === "U" || stringArr[i] === "O" || stringArr[i] === "E"){
-      arr.push("I");
+  if(input === '' || input === null){
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Belum ada text yang di input!',
+    })
+  } else{
+    let stringArr = input.split('');
+    let arr = [];
+    for(let i = 0; i < stringArr.length; i++){
+      if(stringArr[i] === "a" || stringArr[i] === "u" || stringArr[i] === "o" || stringArr[i] === "e"){
+        arr.push("i");
+      }else if( stringArr[i] === "A" || stringArr[i] === "U" || stringArr[i] === "O" || stringArr[i] === "E"){
+        arr.push("I");
+      }
+      else{
+        arr.push(stringArr[i]);
+      }
     }
-    else{
-      arr.push(stringArr[i]);
-    }
+    hasil = arr.join('');
+    console.log(hasil);
+    
+    document.querySelector('.output').innerHTML = `<h4>${hasil}</h4>`;
   }
-  hasil = arr.join('');
-  console.log(hasil);
-  document.querySelector('.output').innerHTML = `<h4>${hasil}</h4>`;
-
   e.preventDefault();
 });
 
 document.getElementById('clear').addEventListener('click', function(e){
-  
   document.querySelector('.output').innerHTML = '';
   document.getElementById('input').value = '';
   e.preventDefault();
